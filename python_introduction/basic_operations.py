9ba3c903cbb4e7c32332752b6b9b134b7571a345
Aneke-Elvis <anekeelvis23@gmail.com>
2025-09-13 19:43:25 +0100

    add python script that does basic arithmetic operations +,- and *

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Python script that performs basic arithmetic operations on two variables
+
+number1 = 10
+number2 = 5
+
+# Addition of two variables
+Addition = number1 + number2
+print("Addition of 10 and 5 is", Addition)
+
+# Subtraction
+Subtraction = number1 - number2
+print("Subtraction of 10 and 5 is", Subtraction)
+
+# Multiplication
+Multiplication = number1 * number2
+print("Multiplication of 10 and 5 is", Multiplication)
