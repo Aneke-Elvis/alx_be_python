2a6ed50bb65cf8b415ded204159a69ffbd2fc49a
Aneke-Elvis <anekeelvis23@gmail.com>
2025-09-13 21:23:06 +0100

    add python scripts that calculate future age base on user input and some assumptions

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,13 @@
+#python script that calculate age base on user input
+# future_age_calculator.py
+
+# Step 1: Ask the user for their current age
+age = int(input("How old are you? "))
+
+# Step 2: Calculate future age in 2050
+future_age = age + 27
+
+# Step 3: Print result
+print(f"In 2050, you will be {future_age} years old.")
+
+#f""→ f-string lets us insert variables inside the string.
