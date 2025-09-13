0481e269e22ac79cb165ca01a0e7461737c493ff
Aneke-Elvis <anekeelvis23@gmail.com>
2025-09-13 20:53:58 +0100

    add Python script that uses basic Arithmetic operation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# Python script that uses basic Arithmetic operation to calculate simple interest from a given principal amount,rate of interest and time.
+principal = 1000 # 1000 dollar
+rate = 0.05 # 5% annual rate
+time = 3 # 3 years
+interest = principal * rate * time
+print("The simple interest is:", interest)
