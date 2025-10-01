e89efc38b94941fc454c5172d8287da920c752c3
Aneke-Elvis <anekeelvis23@gmail.com>
2025-10-01 21:47:35 +0100

    add script for basic math operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 #create a function
-def perform_operation( numb1: float, numb2: float, operation: str):
+def perform_operation( numb1, numb2, operation):
     if operation == "add":
         return numb1 + numb2
     elif operation == "subtract":
